f34d3d201a43a9301fc7b271dcc45fd4bf310eb9
Aleksey <sphota9@gmail.com>
2018-08-14 13:59:24 -0400

    implement SortingEngine with reactive frame dirs

--- src/common.js
+++ src/common.js
@@ -5,3 +5,4 @@ export const emitter = new EventEmitter();
 // Event Codes
 export const EVENT_SOURCE_FOLDER_SELECTED = 'kSourceFolderSelectedEvent';
 export const EVENT_PHOTO_ADDED = 'kPhotoAddedEvent';
+export const EVENT_PHOTO_REMOVED = 'kPhotoRemovedEvent';
--- src/extras/SortingEngine.js
+++ src/extras/SortingEngine.js
@@ -1,56 +1,124 @@
 import { emitter,
   EVENT_SOURCE_FOLDER_SELECTED,
   EVENT_PHOTO_ADDED,
+  EVENT_PHOTO_REMOVED
 } from '../common';
 
 const electron = window.require('electron'); 
 const choker = electron.remote.require('chokidar');
+const moveFile = electron.remote.require('move-file');
 const fs = electron.remote.require('fs');
 const os = window.require('os');
 
 export default class SortingEngine {
-  
   // dirs
   static _sourceDir = null;
   static _sortDir = null;
+  static _sortDirMap = new Map(); // [string: Set<string>]
+  static _dirWatchSet = new Set() // [string: bool]
 
   constructor(source, sort) {
     // set up class methods
     this.routePhoto = this.routePhoto.bind(this);
-    this.onPhotoAddedHandler = this.onPhotoAddedHandler.bind(this);
-
+    this.onSourcePhotoAddedHandler = this.onSourcePhotoAddedHandler.bind(this);
+    this.initWatchDir = this.initWatchDir.bind(this);
+    this.onSourcePhotoRemovedHandler = this.onSourcePhotoRemovedHandler.bind(this);
+    this.onSortedPhotoAddedHandler = this.onSortedPhotoAddedHandler.bind(this);
+    this.onSortedPhotoRemovedHandler = this.onSortedPhotoRemovedHandler.bind(this);
     // init dir strings
     SortingEngine._sourceDir = source;
     SortingEngine._sortDir = sort;
-    // emit source folder event
+    // emit source folder event for subscribing components
     emitter.emit(EVENT_SOURCE_FOLDER_SELECTED, SortingEngine._sourceDir);
+    this.initWatchDir('source', SortingEngine._sourceDir);
+  }
+
+  initWatchDir(type, dir) {
     // initialize dir string to watch
     let watchGlob = null;
     // set the watch dir according to OS
     if (os.platform() === 'darwin') {
-      watchGlob = SortingEngine._sourceDir + '/**/*.jpg';
+      watchGlob = dir + '/*.jpg';
     } else {
-      watchGlob = SortingEngine._sourceDir + '\\**\\*.jpg'
+      watchGlob = dir + '\\*.jpg'
     }
-    // watch the source dir
-    const watcher = choker.watch(watchGlob, {
-      ignored: /(^|[\/\\])\../,
-      persistent: true
-    });
-    // when a file is added, send event to Home
-    watcher.on('add', path => {
-      this.onPhotoAddedHandler(path);
+    console.log(watchGlob);
+    switch (type) {
+      case 'source': {   
+        // watch the source dir
+        const sourceWatcher = choker.watch(watchGlob, {
+          ignored: /(^|[\/\\])\../,
+          persistent: true
+        });
+        // when a file is added, send event to subs
+        sourceWatcher.on('add', path => {
+          this.onSourcePhotoAddedHandler(path);
+        });
+        sourceWatcher.on('unlink', path => {
+          this.onSourcePhotoRemovedHandler(path);
+        })
+        break;
+      }
+      // e.g. _sortDir/000/
+      case 'index': {
+        //TODO: handle when index folder already exists
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir);
+          const filename = dir.replace(/^.*[\\\/]/, '');
+          const index = filename.split('_')[0];
+          SortingEngine._sortDirMap.set(index, new Set());
+        }
+        if (SortingEngine._dirWatchSet.has(dir) === false) {
+          const indexPathWatcher = choker.watch(watchGlob, {
+            ignored: /(^|[\/\\])\../,
+            persistent: true
+          });
+          indexPathWatcher.on('add', path => {
+            this.onSortedPhotoAddedHandler(path);
+          });
+          indexPathWatcher.on('unlink', path => {
+            this.onSortedPhotoRemovedHandler(path);
+          });  
+          SortingEngine._dirWatchSet.add(dir);    
+        }
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  onSourcePhotoAddedHandler(path) {
+    const filename = path.replace(/^.*[\\\/]/, '');
+    const index = filename.split('_')[0];
+    const indexPath = SortingEngine._sortDir +
+      (os.platform() === 'darwin' ? '/' : '\\') +
+      index;
+    // start watching index path
+    this.initWatchDir('index', indexPath);
+    // move photo to index path
+    moveFile(path, 
+      indexPath + (os.platform() === 'darwin' ? '/' : '\\') +
+      filename)
+    .then(() => {
+      console.log(filename + ' moved.');
     });
   }
 
-  onPhotoAddedHandler(path) {
+  onSourcePhotoRemovedHandler(path) {
+    
+  }
+
+  onSortedPhotoAddedHandler(path) {
     emitter.emit(EVENT_PHOTO_ADDED, path);
-    console.log(path);
+  }
+
+  onSortedPhotoRemovedHandler(path) {
+    emitter.emit(EVENT_PHOTO_REMOVED, path);
   }
 
   routePhoto(path) {
 
   }
-
-  // methods
 }
